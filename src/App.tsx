8f9f67a7b5c9518938e533a9fd35834d860a86b0
tony-hungvu <hungvm@avadagroup.com>
2024-05-05 17:50:01 +0700

    todo app reactjs (#1)

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import './App.css';
+
+import React, { useState } from 'react';
+
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+
+interface TodoItem {
+  text: string;
+  isCompleted: boolean;
+}
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<TodoItem[]>([
+    { text: 'Learn about React', isCompleted: false },
+    { text: 'Meet friend for lunch', isCompleted: false },
+    { text: 'Build really cool todo app', isCompleted: false },
+  ]);
+
+  const addTodo = (text: string): void => {
+    const newTodos: TodoItem[] = [...todos, { text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: TodoItem[] = [...todos];
+    newTodos[index] = { ...newTodos[index], isCompleted: true };
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index: number): void => {
+    const newTodos: TodoItem[] = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className='app'>
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
